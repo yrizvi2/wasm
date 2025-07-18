c91de1fb04b852dfd663162fd5b3ed02a8c94067
yrizvi2 <yrizvi@dinmCND12049GB.localdomain>
2025-07-18 01:40:08 -0400

    fix submodule issue

---
+++ wasm-micro-runtime/samples/callback/func.c
@@ -0,0 +1,43 @@
+#include <stdint.h> 
+
+#include <stdio.h> 
+
+  
+
+#define WASM_EXPORT(name) __attribute__((used)) __attribute__((export_name(name))) 
+
+typedef void (*Tfunc)(uint32_t); 
+
+  
+
+// Called from host via indirect call 
+
+void print_num(uint32_t x) { 
+
+    printf("you gave me = %d\n", x); 
+
+} 
+
+  
+
+// Export the function so it's added to the function table 
+
+WASM_EXPORT("print_num_export") 
+
+void print_num_export(uint32_t x) { 
+
+    print_num(x); 
+
+} 
+
+  
+
+// Manually return the index into the table with wasm-objdump
+
+WASM_EXPORT("addr_export") 
+
+uint32_t addr_export() { 
+
+    return 10;
+
+} 
